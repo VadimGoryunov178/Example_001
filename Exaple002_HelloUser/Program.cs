80707ff3bbcc634bd47ab9d739974dd695c952d4
Vadim <79119169948@yandex.ru>
2022-07-08 11:06:22 +0300

    add Exaple002_HelloUser

---
+++ Exaple002_HelloUser/Program.cs
@@ -0,0 +1,14 @@
+﻿internal class NewBaseType
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Введите ваше имя ");
+        string username = Console.ReadLine();
+        Console.Write("Привет, ");
+        Console.Write(username);
+    }
+}
+
+internal class Program : NewBaseType
+{
+}
